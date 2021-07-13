2dfe86b7ee4abe1293da3be64b178ebfe693b1d8
liyang <yang.li@beantechs.com>
2021-07-13 12:20:03 +0800

    add eureka-server-test

--- colin-eureka-server-test/src/main/java/com/colin/EurekaServerTestApplication.java
+++ colin-eureka-server-test/src/main/java/com/colin/EurekaServerTestApplication.java
@@ -2,12 +2,14 @@ package com.colin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class GradleTestApplication {
+@EnableEurekaServer
+public class EurekaServerTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GradleTestApplication.class, args);
+        SpringApplication.run(EurekaServerTestApplication.class, args);
     }
 
 }
